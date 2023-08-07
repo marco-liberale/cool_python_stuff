93809905eb8cdd9c7f737ea062709737b75bd132
Marco Liberale <blackhat@v02d.com>
2023-08-07 06:28:21 -0400

    Create mac_changer.py
    Basic mac changer that I made.

---
+++ mac_changer.py
@@ -0,0 +1,35 @@
+import os
+
+# human made code
+# AI generated comments
+
+# Check if the script is run as root
+if os.geteuid() != 0:
+    print("This script must be run as root!")
+    exit(1)
+else:
+    print("Script is running as root.")
+
+# Print a basic message
+print("marco's basic MAC changer\n enjoy :)")
+
+# Get the MAC address from the user
+mac = input("Enter MAC address\n")
+
+# Get the network interface from the user
+inter = input("Enter interface\n")
+
+# Bring the network interface down
+os.system("sudo ifconfig " + inter + " down")
+
+# Change the MAC address of the network interface
+os.system("sudo ifconfig " + inter + " hw ether " + mac)
+
+# Bring the network interface back up
+os.system("sudo ifconfig " + inter + " up")
+
+# Display the current configuration of the network interface
+os.system("sudo ifconfig " + inter)
+
+# Print a success message
+print("MAC changed :)")
